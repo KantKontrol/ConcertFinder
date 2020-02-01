026050a34f7abc5b284c24a77fd1aef9ad38d11f
Hugo Harley <hpierrel@gmail.com>
2020-02-01 11:43:15 -0500

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //bands in town key 0e0044c7d7a73f73811a78506b57e4ef
+/*
+var queryUrl = "https://get.geojs.io/";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    .then(function (response) {
+        console.log(queryUrl);
+        console.log(response);
+    });
+    */
