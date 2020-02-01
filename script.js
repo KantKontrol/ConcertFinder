974ff969bc082e0b30c615a80b4a89ce26ce0549
Hugo Harley <hpierrel@gmail.com>
2020-02-01 13:10:41 -0500

    merging1

--- script.js
+++ script.js
@@ -13,36 +13,23 @@ $("#class").on("click", function () {
     getBandsInTownEvents(bandName);
 });
 
-<<<<<<< HEAD
 function getBandsInTownEvents(bandName, date) {
-    var app_id = "0e0044c7d7a73f73811a78506b57e4ef";
 
+    var app_id = "0e0044c7d7a73f73811a78506b57e4ef";
     var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=" + app_id;
-=======
-function getBandsInTownEvents(bandName, date){
-
-    var app_id = "0e0044c7d7a73f73811a78506b57e4ef";
-    var queryURL = "https://rest.bandsintown.com/artists/" + bandName+ "/events?app_id=" + app_id;
->>>>>>> 1dd38414ea0cd020679340df29f2f42f1cd236f5
 
     console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
-<<<<<<< HEAD
     }).then(function (response) {
-        console.log(response);
-    });
-=======
-    }).then(function(response){
 
-        for(let i=0;i < response.length;i++){
-            
+        for (let i = 0; i < response.length; i++) {
+
             console.log(response[i]);
         }
-        
-    });    
->>>>>>> 1dd38414ea0cd020679340df29f2f42f1cd236f5
+
+    });
 }
 
